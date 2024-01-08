1c9b1c9306a92a3c11168fd5381232d0f742fd6f
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-01-08 13:47:53 +0100

    updt 05

---
+++ LabirintusGUI/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace LabirintusGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUI01 = new Label();
+            cbxOszlop = new ComboBox();
+            cbxSor = new ComboBox();
+            btnLetrehozas = new Button();
+            btnMentes = new Button();
+            cbxIndex = new ComboBox();
+            pnlMain = new Panel();
+            SuspendLayout();
+            // 
+            // lblUI01
+            // 
+            lblUI01.AutoSize = true;
+            lblUI01.Location = new Point(24, 34);
+            lblUI01.Name = "lblUI01";
+            lblUI01.Size = new Size(227, 21);
+            lblUI01.TabIndex = 0;
+            lblUI01.Text = "labirintus mérete [sor x oszlop]:";
+            // 
+            // cbxOszlop
+            // 
+            cbxOszlop.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxOszlop.FormattingEnabled = true;
+            cbxOszlop.Location = new Point(318, 31);
+            cbxOszlop.Name = "cbxOszlop";
+            cbxOszlop.Size = new Size(55, 29);
+            cbxOszlop.TabIndex = 1;
+            // 
+            // cbxSor
+            // 
+            cbxSor.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxSor.FormattingEnabled = true;
+            cbxSor.Location = new Point(257, 31);
+            cbxSor.Name = "cbxSor";
+            cbxSor.Size = new Size(55, 29);
+            cbxSor.TabIndex = 1;
+            // 
+            // btnLetrehozas
+            // 
+            btnLetrehozas.Location = new Point(17, 78);
+            btnLetrehozas.Name = "btnLetrehozas";
+            btnLetrehozas.Size = new Size(234, 29);
+            btnLetrehozas.TabIndex = 2;
+            btnLetrehozas.Text = "induló labirintus létrehozása";
+            btnLetrehozas.UseVisualStyleBackColor = true;
+            // 
+            // btnMentes
+            // 
+            btnMentes.Location = new Point(257, 78);
+            btnMentes.Name = "btnMentes";
+            btnMentes.Size = new Size(154, 29);
+            btnMentes.TabIndex = 2;
+            btnMentes.Text = "labirintus mentése";
+            btnMentes.UseVisualStyleBackColor = true;
+            // 
+            // cbxIndex
+            // 
+            cbxIndex.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbxIndex.FormattingEnabled = true;
+            cbxIndex.Location = new Point(417, 78);
+            cbxIndex.Name = "cbxIndex";
+            cbxIndex.Size = new Size(55, 29);
+            cbxIndex.TabIndex = 1;
+            // 
+            // pnlMain
+            // 
+            pnlMain.Location = new Point(12, 126);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(460, 423);
+            pnlMain.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 561);
+            Controls.Add(pnlMain);
+            Controls.Add(btnMentes);
+            Controls.Add(btnLetrehozas);
+            Controls.Add(cbxSor);
+            Controls.Add(cbxIndex);
+            Controls.Add(cbxOszlop);
+            Controls.Add(lblUI01);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "MainForm";
+            Text = "Labirintus készítő";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUI01;
+        private ComboBox cbxOszlop;
+        private ComboBox cbxSor;
+        private Button btnLetrehozas;
+        private Button btnMentes;
+        private ComboBox cbxIndex;
+        private Panel pnlMain;
+    }
+}
---
+++ LabirintusGUI/MainForm.cs
@@ -0,0 +1,97 @@
+using System.Windows.Forms;
+
+namespace LabirintusGUI
+{
+    public partial class MainForm : Form
+    {
+        public CheckBox[,] CbxMatrix { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitDropDowns();
+            btnLetrehozas.Click += BtnLetrehozas_Click;
+            btnMentes.Click += BtnMentes_Click;
+        }
+
+        private void BtnMentes_Click(object? sender, EventArgs e)
+        {
+            try
+            {
+                if (CbxMatrix is null) throw new Exception("még nem hoztál létre labirintust!");
+
+                using StreamWriter sw = new($@"..\..\..\sae\Lab{cbxIndex.SelectedItem}.txt");
+                for (int s = 0; s < CbxMatrix.GetLength(0); s++)
+                {
+                    string sor = string.Empty;
+                    for (int o = 0; o < CbxMatrix.GetLength(1); o++)
+                    {
+                        sor += CbxMatrix[s, o].Checked ? 'X' : ' ';
+                    }
+                    sw.WriteLine(sor);
+                }
+
+                MessageBox.Show(
+                    caption: "siker!",
+                    text: "Az állomány mentése sikeres!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    caption: "hiba!",
+                    text: ex.Message);
+            }
+        }
+
+        private void BtnLetrehozas_Click(object? sender, EventArgs e)
+        {
+            pnlMain.Controls.Clear();
+
+            CbxMatrix = new CheckBox[
+                (int)cbxSor.SelectedItem, 
+                (int)cbxOszlop.SelectedItem];
+
+            for (int s = 0; s < CbxMatrix.GetLength(0); s++)
+            {
+                for (int o = 0; o < CbxMatrix.GetLength(1); o++)
+                {
+                    CbxMatrix[s, o] = new CheckBox()
+                    {
+                        Bounds = new(
+                            x: o * 20,
+                            y: s * 20,
+                            width: 20,
+                            height: 20),
+                        Enabled = 
+                               s != 0 
+                            && o != 0 
+                            && s != CbxMatrix.GetLength(0) - 1
+                            && o != CbxMatrix.GetLength(1) - 1,
+                    };
+                    CbxMatrix[s, o].Checked = !CbxMatrix[s,o].Enabled;
+                    pnlMain.Controls.Add(CbxMatrix[s, o]);
+                }
+            }
+            CbxMatrix[1, 0].Checked = false;
+            CbxMatrix[CbxMatrix.GetLength(0) - 2, CbxMatrix.GetLength(1) - 1].Checked = false;
+        }
+
+        public void InitDropDowns()
+        {
+            for (int d = 1; d <= 20; d++)
+            {
+                if (d >= 5)
+                {
+                    cbxSor.Items.Add(d);
+                    cbxOszlop.Items.Add(d);
+                }
+                if (d <= 16) cbxIndex.Items.Add(d);
+            }
+            cbxSor.SelectedItem = 12;
+            cbxOszlop.SelectedItem = 12;
+            cbxIndex.SelectedItem = 3;
+        }
+    }
+
+
+}
