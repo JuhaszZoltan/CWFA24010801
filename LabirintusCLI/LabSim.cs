78107e76790722ba31097aad6ad6297521f772dd
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-01-08 10:26:38 +0100

    updt 2

--- LabirintusCLI/LabSim.cs
+++ LabirintusCLI/LabSim.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading;
 
 namespace LabirintusCLI
 {
@@ -12,8 +8,8 @@ namespace LabirintusCLI
         private char[,] lab;
 
         public bool KeresesKesz { get; set; }
-        public int KijaratOszlopindex { get; }
-        public int KijaratSorindex { get; }
+        public int KijaratOszlopIndex { get; }
+        public int KijaratSorIndex { get; }
         public bool NincsMegoldas { get; set; }
         public int OszlopokSzama { get; }
         public int SorokSzama { get; }
@@ -25,15 +21,66 @@ namespace LabirintusCLI
         }
         private void FeltoltLab()
         {
-            throw new NotImplementedException();
+            int s = 0;
+            foreach (string sor in adatsorok)
+            {
+                int o = 0;
+                foreach (char karakter in sor)
+                {
+                    lab[s, o] = karakter;
+                    o++;
+                }
+                s++;
+            }
         }
         public void KiirLab()
         {
-            throw new NotImplementedException();
+            for (int s = 0; s < lab.GetLength(0); s++)
+            {
+                for (int o = 0; o < lab.GetLength(1); o++)
+                {
+                    Console.Write(lab[s,o]);
+                }
+                Console.Write("\n");
+            }
         }
         public void Utkeres()
         {
-            throw new NotImplementedException();
+            Thread.Sleep(3000);
+            Console.Clear();
+            KiirLab();
+
+            bool keresesKesz = false;
+            bool nincsMegoldas = false;
+            int r = 1;
+            int c = 0;
+            while (!keresesKesz && !nincsMegoldas)
+            {
+                Console.SetCursorPosition(c, r);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                lab[r, c] = 'o';
+                Console.Write('o');
+                if (lab[r, c + 1] == ' ') c++;
+                else if (lab[r + 1, c] == ' ') r++;
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    lab[r, c] = '-';
+                    Console.Write('-');
+                    if (lab[r, c - 1] == 'o') c--;
+                    else r--;
+                }
+                keresesKesz = r == KijaratSorIndex && c == KijaratOszlopIndex;
+                if (keresesKesz)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    lab[r, c] = 'o';
+                    Console.Write('o');
+                }
+                nincsMegoldas = r == 1 && c == 0;
+                Thread.Sleep(500);
+            }
         }
 
         public LabSim(string forras)
@@ -42,8 +89,9 @@ namespace LabirintusCLI
             BeolvasAdatsorok(forras);
             SorokSzama = adatsorok.Count;
             OszlopokSzama = adatsorok[0].Length;
-            KijaratOszlopindex = OszlopokSzama - 1;
-            KijaratSorindex = SorokSzama - 2;
+            KijaratOszlopIndex = OszlopokSzama - 1;
+            KijaratSorIndex = SorokSzama - 2;
+            lab = new char[SorokSzama, OszlopokSzama];
             FeltoltLab();
         }
     }
--- LabirintusCLI/Program.cs
+++ LabirintusCLI/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("hello world!");
+﻿using LabirintusCLI;
+
+LabSim lab = new(@"..\..\..\src\Lab1.txt");
+Console.WriteLine("5. feladat: labirintus adatai\n" +
+    $"\tSorok száma: {lab.SorokSzama}\n" +
+    $"\tOszlopok száma: {lab.OszlopokSzama}\n" +
+    $"\tKijárat indexe: sor: {lab.KijaratSorIndex} oszlop: {lab.KijaratOszlopIndex}");
+Console.WriteLine("6. feladat: a labirintus");
+lab.KiirLab();
+lab.Utkeres();
